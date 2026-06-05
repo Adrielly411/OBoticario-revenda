3d812dae077419683a581b5145cf4ba706928639
Cesar Wrzecionek de Brito <cesar.wrzecionek@gmail.com>
2026-06-04 21:08:37 -0300

    Create the admin pages:
    - Create the main page to edit and delete
    - Create the navbar with the links
    - Create the page to create product
    - Create the page to edit product
    - Create the page to reports

--- src/main/java/br/com/oboticariorevenda/oboticario_revenda/controller/IndexController.java
+++ src/main/java/br/com/oboticariorevenda/oboticario_revenda/controller/IndexController.java
@@ -9,4 +9,24 @@ public class IndexController {
     public String getIndex() {
         return "index";
     }
+
+    @GetMapping("/admin")
+    public String getAdminIndex() {
+        return "/admin/index";
+    }
+
+    @GetMapping("/admin/relatorio")
+    public String getAdminReport() {
+        return "/admin/report";
+    }
+
+    @GetMapping("/admin/criar-produto")
+    public String getAdminCreateProduct() {
+        return "/admin/create-product";
+    }
+
+    @GetMapping("/admin/editar-produto")
+    public String getAdminEditProduct() {
+        return "/admin/edit-product";
+    }
 }
